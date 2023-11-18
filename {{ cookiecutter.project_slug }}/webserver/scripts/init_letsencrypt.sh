c93275c6bf23628d3836460bdc06897fffbe177f
Greg Chatzinikolaou <greghatzis@gmail.com>
2023-11-18 22:52:45 +0200

    Add cookiecutter prefix.

--- {{ cookiecutter.project_slug }}/webserver/scripts/init_letsencrypt.sh
+++ {{ cookiecutter.project_slug }}/webserver/scripts/init_letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=({{ domain_name }} {{ domain_name }})
+domains=({{ cookiecutter.domain_name }} {{ cookiecutter.domain_name }})
 rsa_key_size=4096
 data_path="./webserver/certbot"
-email="{{ email_address }}" # Adding a valid address is strongly recommended
+email="{{ cookiecutter.email_address }}" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
